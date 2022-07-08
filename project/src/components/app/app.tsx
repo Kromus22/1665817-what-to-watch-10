d758c293158a791798855fd6709511341b21b1fe
Kromus22 <kromus2@gmail.com>
2022-07-08 09:45:53 +0500

    +main page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type MainPageProps = {
+  title: string;
+  genres: string;
+  releaseDate: number;
+}
+
+function App({ title, genres, releaseDate }: MainPageProps): JSX.Element {
+  return (<MainPage title={title} genres={genres} releaseDate={releaseDate} />);
 }
 
 export default App;
