40a1c4ad23534c34aa5490ca6cde59af7f25fd2c
Kromus22 <kromus2@gmail.com>
2022-07-13 08:19:48 +0500

    +routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import MyList from '../../pages/my-list-page/my-list-page';
+import MoviePage from '../../pages/movie-page/movie-page';
+import MovieReviewsPage from '../../pages/movie-reviews-page/movie-reviews-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import EmptyPage from '../../pages/empty-page/empty-page';
+import PrivateRoute from '../private-route/private-route';
 
 type MainPageProps = {
   title: string;
@@ -7,7 +16,39 @@ type MainPageProps = {
 }
 
 function App({ title, genre, releaseDate }: MainPageProps): JSX.Element {
-  return (<MainPage title={title} genre={genre} releaseDate={releaseDate} />);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}
+          element={<MainPage title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.SignIn}
+          element={<LoginPage />}
+        />
+        <Route path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Film}
+          element={<MoviePage title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.AddReview}
+          element={<MovieReviewsPage title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.Player}
+          element={<PlayerPage />}
+        />
+        <Route path="*"
+          element={<EmptyPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
